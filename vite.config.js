b587877729c2dd5df07a224328b64391f9e4a2e1
(John) Martin Säwström <68476975+realelidooro@users.noreply.github.com>
2026-05-28 18:43:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  plugins: [react()],
+  root: 'client',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
